ba57beec852905eb912b6f9c003d5d4be48ee63e
ykmshhh <mayak.ol.02.10@gmail.com>
2025-04-01 22:42:06 +0300

    initial commit for Spring Boot project setup

---
+++ backend/src/main/java/com/pickme/PickMeApplication.java
@@ -0,0 +1,13 @@
+package com.pickme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickMeApplication.class, args);
+    }
+
+}
