00c0c607f749d5acc9c161f6546c38614322a672
ykmshhh <yakukhno.maria@lll.kpi.ua>
2025-05-19 20:19:52 +0300

    feat: add BookDTO and dependency in backend pom.xml

---
+++ backend/src/main/java/com/pickme/dto/book/BookDTO.java
@@ -0,0 +1,18 @@
+package com.pickme.dto.book;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDTO {
+
+    private String title;
+    private String author;
+    private String description;
+
+}
